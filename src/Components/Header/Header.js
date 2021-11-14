669f065f55f1e859508e48ea7a84c82f850c0fe1
kamoun <kamoun.malik@gmail.com>
2021-11-14 20:47:57 +0100

    Add header style

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,8 @@
+import './Header.scss'
 export default function Header () {
     return (
         <>
-            <p>
+            <p className='content'>
             C'est la faute à Florent
             </p>
         </>
