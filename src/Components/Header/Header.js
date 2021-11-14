61e2a325b737022e736f38c1b3781dfc937bb1c9
Malik Kamoun <kamoun.malik@gmail.com>
2021-11-14 18:49:42 +0100

    Start project

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+export default function Header () {
+    return (
+        <>
+            <p>
+            C'est la faute à Florent
+            </p>
+        </>
+    )
+}
---
+++ src/Pages/About/About.js
@@ -0,0 +1,9 @@
+import Header from "../../Components/Header"
+export default function About() {
+    return (
+        <div>
+            <h1>Bienvenue !</h1>
+            <Header></Header>
+        </div>
+    )
+}
